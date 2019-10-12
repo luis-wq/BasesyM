714c0ce4f14b17b6d2b7888ccf150be1ca562a2e
jorge ruiz <jorge_ziur@hotmail.com>
2019-10-11 23:27:58 -0500

    Corregido aumento de precios por cantidad y pesos por cantidad

--- BasesYMolduras/AgregarCotizacion.cs
+++ BasesYMolduras/AgregarCotizacion.cs
@@ -371,7 +371,6 @@ namespace BasesYMolduras
                 row["CANT"] = txtCantidad.Text;
 
 
-            datosPreciosSeleccion = null;
                 datosPreciosSeleccion = BD.consultaPrecio(comboModelo.GetItemText(comboModelo.SelectedItem).ToString(),
                     Convert.ToInt32(datosTamanio.Rows[comboTamanio.SelectedIndex]["id_tamano"]),
                     Convert.ToInt32(datosMateriales.Rows[comboMaterial.SelectedIndex]["id_material"]),
@@ -380,15 +379,15 @@ namespace BasesYMolduras
             
                 if (datosClientes.Rows[comboCliente.SelectedIndex]["tipo_cliente"].Equals("PUBLICO"))
                 {
-                    row["PRECIO"] = "$" + datosPreciosSeleccion.Rows[0]["precio_publico"];
+                    row["PRECIO"] = "$" + Convert.ToDouble(datosPreciosSeleccion.Rows[0]["precio_publico"]) * Convert.ToDouble(row["CANT"]);
                 }
                 else if (datosClientes.Rows[comboCliente.SelectedIndex]["tipo_cliente"].Equals("FRECUENTE"))
                 {
-                    row["PRECIO"] = "$" + datosPreciosSeleccion.Rows[0]["precio_frecuente"];
+                    row["PRECIO"] = "$" + Convert.ToDouble(datosPreciosSeleccion.Rows[0]["precio_frecuente"]) * Convert.ToDouble(row["CANT"]);
                 }
                 else if (datosClientes.Rows[comboCliente.SelectedIndex]["tipo_cliente"].Equals("MAYORISTA"))
                 {
-                    row["PRECIO"] = "$" + datosPreciosSeleccion.Rows[0]["precio_mayorista"];
+                    row["PRECIO"] = "$" + Convert.ToDouble(datosPreciosSeleccion.Rows[0]["precio_mayorista"]) * Convert.ToDouble(row["CANT"]);
                 }
                 row["CANTA"] = Convert.ToInt32(datosPreciosSeleccion.Rows[0]["cantidad"]);
 
@@ -400,7 +399,8 @@ namespace BasesYMolduras
                 lista.Columns[lista.Columns["CANT"].Index].Width = 55;
                 lista.Columns[lista.Columns["PRECIO"].Index].Width = 65;
                 lista.Columns[lista.Columns["PESO"].Index].Width = 65;
-                row["PESO"] = datosPreciosSeleccion.Rows[0]["peso"] + "kg";
+                Double valor = Convert.ToDouble(datosPreciosSeleccion.Rows[0]["peso"]) * Convert.ToDouble(row["CANT"]);
+                row["PESO"] = string.Format("{0:n2}", (Math.Truncate(valor * 100) / 100)) + "kg";
                 row["ID"] = datosPreciosSeleccion.Rows[0]["id_producto"];
                 row["Id_color"] = datosColores.Rows[comboColor.SelectedIndex]["id_color"];
                 row["Id_tipo"] = datosTipo.Rows[comboTipo.SelectedIndex]["id_tipo"];
